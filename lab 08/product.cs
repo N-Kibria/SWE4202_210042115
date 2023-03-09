bebf5faf9cd09e67941147006afb590f045c3817
N-Kibria <nafisa21@iut-dhaka.edu>
2023-03-09 08:57:59 +0600

    Created product Class

---
+++ lab 08/product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_08
+{
+    internal class product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public double price { get; set; }
+        public int qty { get; set; }
+
+        public int min_qty { get; set; }
+        public int req_amount { get; set;}
+
+        public product(int id, string name, double price, int qty, int min_qty, int req_amount)
+        {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+            this.qty = qty;
+            this.min_qty = min_qty;
+            this.req_amount = req_amount;
+        }
+        public void auto_req()
+        {
+            qty += req_amount;
+
+        }
+        public void req(int amount)
+        {
+            qty += amount;
+        }
+
+    }
+}
