86bd17631f8caac98c564b2f91eb1421b69e1fc4
N-Kibria <nafisa21@iut-dhaka.edu>
2023-03-09 09:23:30 +0600

    created employee class(abstract)

---
+++ lab 08/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_08
+{
+    internal abstract class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double base_salary { get; set; }
+        public int inventory { get; set; }
+        public Employee() 
+        {
+            inventory = new inventory();
+
+        }
+        public abstract void get_salary(int id);
+        
+
+        
+    }
+}
