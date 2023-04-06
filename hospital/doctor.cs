1fd70897d6b9faedd434bdfb2402b335fa0dd9ec
N-Kibria <nafisa21@iut-dhaka.edu>
2023-04-06 09:54:52 +0600

    created doctor class

---
+++ hospital/doctor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hospital
+{
+    internal class doctor : employee
+    {
+        public string Department { get; set; }
+        public string Position { get; set; }
+        public List<string> Degrees { get; set; }
+        public List<string> WorksWith { get; set; }
+
+        public doctor(string name, string address, string phone, string email, DateTime hireDate, string department, string position, List<string> degrees, List<string> worksWith) : base(name, address, phone, email, hireDate)
+        {
+            Department = department;
+            Position = position;
+            Degrees = degrees;
+            WorksWith = worksWith;
+        }
+
+        public override int CalculateYearsOfService()
+        {
+            return DateTime.Now.Year - HireDate.Year;
+        }
+
+        public override int ShowDetails()
+        {
+            return CalculateYearsOfService();
+        }
+    }
+}
---
+++ hospital/managementstaff.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hospital
+{
+    internal class managementstaff : employee
+    {
+        public string Department { get; set; }
+        public string Position { get; set; }
+
+        public managementstaff(string name, string address, string phone, string email, DateTime hireDate, string department, string position) : base(name, address, phone, email, hireDate)
+        {
+            Department = department;
+            Position = position;
+        }
+
+        public override int CalculateYearsOfService()
+        {
+            return DateTime.Now.Year - HireDate.Year;
+        }
+
+        public override int ShowDetails()
+        {
+            return CalculateYearsOfService();
+        }
+    }
+}
