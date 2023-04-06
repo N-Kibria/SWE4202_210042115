8e6b698dcfca8fcab54f2b5619154c12c0333909
N-Kibria <nafisa21@iut-dhaka.edu>
2023-04-06 09:04:12 +0600

    created the project

---
+++ hospital/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hospital
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
