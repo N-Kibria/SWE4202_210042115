f8e16ae79ab1d0be95eab20d1b9fb46515eab9bd
N-Kibria <nafisa21@iut-dhaka.edu>
2023-04-06 09:45:55 +0600

    added nurse class

--- hospital/Program.cs
+++ hospital/Program.cs
@@ -10,6 +10,7 @@ namespace hospital
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
--- hospital/employee.cs
+++ hospital/employee.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace hospital
 {
-    internal class employee
+    internal abstract class employee
     {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public DateTime HireDate { get; set; }
+
+        public employee(string name, string address, string phone, string email, DateTime hireDate)
+        {
+            Name = name;
+            Address = address;
+            Phone = phone;
+            Email = email;
+            HireDate = hireDate;
+        }
+        public abstract int CalculateYearsOfService();
+
+        public abstract int ShowDetails();
     }
 }
