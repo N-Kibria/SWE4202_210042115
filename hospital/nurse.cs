474f5eea6983fdc52b5b9116ff04663c13d7ab5d
N-Kibria <nafisa21@iut-dhaka.edu>
2023-04-06 09:45:39 +0600

    added

---
+++ hospital/nurse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hospital
+{
+    internal class nurse : employee
+    {
+        public string position { get; set; }
+        public string department { get; set; }
+        
+        
+        
+
+        public nurse(string name, string address, string phone, string email, DateTime hireDate, string position, string department) : base(name, address, phone, email, hireDate)
+        {
+            this.position = position;
+            this.department = department;
+        }
+        public override int CalculateYearsOfService()
+        {
+            return DateTime.Now.Year - HireDate.Year;
+        }
+        public override int ShowDetails()
+        {
+            return CalculateYearsOfService();
+        }
+    }
+}
